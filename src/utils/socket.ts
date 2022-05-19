eee29554fef87eb1cff13f60f288e525e7315c80
Miguel <46081875+nejomi@users.noreply.github.com>
2022-05-19 20:52:56 +0800

    update server url to use deployment

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -5,7 +5,7 @@ import {
 } from '../types/Types';
 
 // const URL = 'http://localhost:5000';
-export const URL = 'http://ecollect.site';
+export const URL = 'https://ecollect.site';
 const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(URL, {
   autoConnect: false,
 });
